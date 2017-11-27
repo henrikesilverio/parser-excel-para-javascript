6b31afd55ef72e48bd4204a2c7e0e93f88373838
Henrique Silverio <henrique.silverio@db1.com.br>
2017-11-27 15:52:58 -0200

    Inclusão do coveralls e primeiro build

--- test/analise-lexica-test.js
+++ test/analise-lexica-test.js
@@ -1,17 +1,18 @@
 'use strict'
 
+const assert = require('chai').assert;
 const lexica = require('../src/analise-lexica');
 
 describe("AnaliseLexica", function() {
     it("Deve retornar uma lista com 4 tokens", function() {
-        expect(lexica('SUM(A2)').length).toEqual(4);
+        assert.equal(lexica('SUM(A2)').length, 4);
     });
 
     it("Deve conter a sequência de tokens SUM, (, ID, )", function() {
         var tokens = lexica('SUM(A2)');
-        expect(tokens[0]).toEqual("SUM");
-        expect(tokens[1]).toEqual("(");
-        expect(tokens[2]).toEqual("ID");
-        expect(tokens[3]).toEqual(")");
+        assert.equal(tokens[0], "SUM");
+        assert.equal(tokens[1], "(");
+        assert.equal(tokens[2], "ID");
+        assert.equal(tokens[3], ")");
     });
 });
